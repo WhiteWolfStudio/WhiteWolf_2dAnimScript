59239ab1894fdb07ca9553200408be3a84cc5bde
White Wolf Game Studio <58172779+WhiteWolfStudio@users.noreply.github.com>
2022-02-12 16:33:26 +0100

    Add files via upload

---
+++ WW_2dAnimScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.IO;
+
+namespace WhiteWolf {
+
+    public class WW_2dAnimScript : MonoBehaviour {
+
+        /* folder/file */
+        public string resourcePath;
+        public Sprite[] animFrames;
+
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+
+        SpriteRenderer sr;
+        int frame;
+
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+
+        void Start(){
+
+            sr = this.gameObject.GetComponent<SpriteRenderer>();
+            animFrames = Resources.LoadAll<Sprite>( resourcePath );
+            
+        }
+
+        void Update() => sr.sprite = animFrames[ frame ];
+
+        /*––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––*/
+
+        public void SetFrame( int frame ) => this.frame = frame;
+        public void PlusFrame() => this.frame += 1;
+
+    }
+
+}
